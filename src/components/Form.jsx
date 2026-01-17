e7efce4b66bb977ed7cd3a2b09383c26067546e7
Drogshell <89346155+Drogshell@users.noreply.github.com>
2026-01-17 23:25:06 +1100

    Maps & Cities
    - Fleshed out the map functionality
    - You can click on the map and add a new visited city
    - All data is reflected in the fake API
    - You can delete visited cities

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,16 @@
-// "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=0&longitude=0"
-
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import styles from "./Form.module.css";
 import Button from "./Button.jsx";
 import { useNavigate } from "react-router-dom";
+import { useURLposition } from "../hooks/useURLposition.js";
+import Message from "./Message.jsx";
+import Spinner from "./Spinner.jsx";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { useCities } from "../context/CitiesContext.jsx";
+
+const BASE_URL = "https://api.bigdatacloud.net/data/reverse-geocode-client";
 
 export function convertToEmoji(countryCode) {
   const codePoints = countryCode
@@ -15,15 +21,77 @@ export function convertToEmoji(countryCode) {
 }
 
 function Form() {
+  const [lat, lng] = useURLposition();
   const [cityName, setCityName] = useState("");
   const [country, setCountry] = useState("");
   const [date, setDate] = useState(new Date());
   const [notes, setNotes] = useState("");
+  const [isLoadingGeo, setIsLoadingGeo] = useState(false);
+  const [emoji, setEmoji] = useState("");
+  const [geoError, setGeoError] = useState("");
 
+  const { isLoading, createCity } = useCities();
   const navigate = useNavigate();
 
+  useEffect(
+    function () {
+      if (!lat && !lng) return;
+
+      async function fetchCityData() {
+        try {
+          setIsLoadingGeo(true);
+          setGeoError("");
+          const res = await fetch(
+            `${BASE_URL}?latitude=${lat}&longitude=${lng}`,
+          );
+          const data = await res.json();
+          if (!data.countryCode) throw new Error("Please click on a city!");
+
+          setCityName(data.city || data.locality || "");
+          setCountry(data.countryName);
+          setEmoji(convertToEmoji(data.countryCode));
+        } catch (err) {
+          setGeoError(err.message);
+        } finally {
+          setIsLoadingGeo(false);
+        }
+      }
+      fetchCityData();
+    },
+    [lat, lng],
+  );
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    if (!cityName || !date) return;
+
+    const newCity = {
+      cityName,
+      country,
+      emoji,
+      date,
+      notes,
+      position: { lat, lng },
+    };
+
+    await createCity(newCity);
+    navigate("/app/cities");
+  }
+
+  if (!lat && !lng)
+    return <Message message="Start by clicking somewhere on the map!" />;
+
+  if (isLoadingGeo) return <Spinner />;
+
+  if (geoError) {
+    return <Message message={geoError} />;
+  }
+
   return (
-    <form className={styles.form}>
+    <form
+      className={`${styles.form} ${isLoading ? styles.loading : ""}`}
+      onSubmit={handleSubmit}
+    >
       <div className={styles.row}>
         <label htmlFor="cityName">City name</label>
         <input
@@ -31,15 +99,16 @@ function Form() {
           onChange={(e) => setCityName(e.target.value)}
           value={cityName}
         />
-        {/* <span className={styles.flag}>{emoji}</span> */}
+        <span className={`${styles.flag} flag-emoji`}>{emoji}</span>
       </div>
 
       <div className={styles.row}>
         <label htmlFor="date">When did you go to {cityName}?</label>
-        <input
+        <DatePicker
           id="date"
-          onChange={(e) => setDate(e.target.value)}
-          value={date}
+          showMonthYearDropdown={true}
+          onChange={(date) => setDate(date)}
+          selected={date}
         />
       </div>
 
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,27 +1,83 @@
 import styles from "./Map.module.css";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMap,
+  useMapEvents,
+} from "react-leaflet";
+import { useEffect, useState } from "react";
+import { useCities } from "../context/CitiesContext.jsx";
+import { useGeolocation } from "../hooks/useGeolocation.js";
+import Button from "./Button.jsx";
+import { useURLposition } from "../hooks/useURLposition.js";
 
 function Map() {
-  const [searchParams, setSearchParams] = useSearchParams();
+  const { cities } = useCities();
+  const [mapPosition, setMapPosition] = useState([-37, 144]);
+  const [mapLat, mapLng] = useURLposition();
+  const {
+    isLoading: isLoadingPos,
+    position: geoPos,
+    getPosition,
+  } = useGeolocation();
 
-  const navigate = useNavigate();
+  useEffect(
+    function () {
+      if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+    },
+    [mapLat, mapLng],
+  );
 
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  useEffect(
+    function () {
+      if (geoPos) {
+        setMapPosition([geoPos.lat, geoPos.lng]);
+      }
+    },
+    [geoPos],
+  );
 
   return (
-    <div
-      className={styles.mapContainer}
-      onClick={() => {
-        navigate("form");
-      }}
-    >
-      <h1>MAP</h1>
-      <h2>
-        {lat} + {lng}
-      </h2>
+    <div className={styles.mapContainer}>
+      {!geoPos && (
+        <Button type="position" onClick={getPosition}>
+          {isLoadingPos ? "...Loading" : "Get your current position"}
+        </Button>
+      )}
+      <MapContainer center={mapPosition} zoom={12} className={styles.map}>
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span className="flag-emoji">{city.emoji}</span>
+              <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+        <ZipToCenter position={mapPosition} />
+        <DetectClick />
+      </MapContainer>
     </div>
   );
+}
+
+function ZipToCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
+}
+
+function DetectClick() {
+  const navigate = useNavigate();
+  useMapEvents({
+    click: (e) => navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`),
+  });
 }
 
 export default Map;
